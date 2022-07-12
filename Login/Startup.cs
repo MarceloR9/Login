ac0863edffed79105193f9a7a0a50b8c7ca17cbe
PC <PC@DESKTOP-6N49J6I>
2022-07-12 02:46:29 -0300

    Adicionar arquivos de projeto.

---
+++ Login/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace Login
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Login}/{id}");
+            });
+        }
+    }
+}
